2d9c4834ec885d56bd9202fce3344c6d37f71bb6
Jeremy Goodman <jeremygoodman52@gmail.com>
2022-11-16 15:25:04 -0600

    7 fish on array

---
+++ scripts/database.js
@@ -0,0 +1,45 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const database = {
+    fish: [
+        {
+            name: "Bart",
+            food: "crustaceans",
+            size: 3
+        }
+        {
+            name: "jason",
+            food: "plankton",
+            size: 7
+        }
+        {
+            name: "kelly",
+            food: "crustaceans",
+            size: 21
+        }
+        {
+            name: "tyler",
+            food: "plankton",
+            size: 8
+        }
+        {
+            name: "sally",
+            food: "plankton",
+            size: 1
+        }
+        {
+            name: "Carl",
+            food: "Whales",
+            size: 952
+        }
+        {
+            name: "Veronica",
+            food: "crustaceans",
+            size: 4,
+        }
+    ]
+}
